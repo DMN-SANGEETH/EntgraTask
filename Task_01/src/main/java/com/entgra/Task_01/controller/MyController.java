a72242245c909a0b082695467357275c30902276
DMN-SANGEETH <dmnsageeth@gmail.com>
2023-02-08 22:13:21 +0530

    cmpleted task 01

--- Task_01/src/main/java/com/entgra/Task_01/controller/MyController.java
+++ Task_01/src/main/java/com/entgra/Task_01/controller/MyController.java
@@ -1,9 +1,33 @@
 package com.entgra.Task_01.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.entgra.Task_01.model.Device;
+import com.entgra.Task_01.service.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping
 public class MyController {
-    //@GetMapping("/")
+    @Autowired
+    private DeviceService deviceService;
+    @GetMapping("/getAllDevice")
+    public Iterable<Device> getAllDevice(){
+        return deviceService.getAllDevice();
+    }
+    @GetMapping("/getDevice/{id}")
+    public Device getDevice(@PathVariable  Integer id){
+        return deviceService.getDevice(id);
+    }
+    @PostMapping("/addDevice")
+    public Device addDevice(@RequestBody Device device){
+        return deviceService.addDevice(device);
+    }
+    @PutMapping("/updateDevice")
+    public Device updateDevice(@RequestBody Device device){
+        return deviceService.updateDevice(device);
+    }
+    @DeleteMapping("/deleteDevice/{id}")
+    public void deleteDevice(@PathVariable Integer id){
+        deviceService.deleteDevice(id);
+    }
 }
--- Task_01/src/main/java/com/entgra/Task_01/service/DeviceService.java
+++ Task_01/src/main/java/com/entgra/Task_01/service/DeviceService.java
@@ -1,13 +1,31 @@
 package com.entgra.Task_01.service;
 
 import com.entgra.Task_01.dao.DeviceDAO;
+import com.entgra.Task_01.model.Device;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class DeviceService {
     @Autowired
-    DeviceDAO dao;
+    private  DeviceDAO dao;
+    public Iterable<Device>  getAllDevice(){
+        return dao.findAll();
+    }
+    public Device getDevice(Integer id){
+        return dao.findById(id).get();
+    }
+    public Device addDevice(Device device){
+        return dao.save(device);
+//        return "New mobile phone added to the data base";
+    }
+    public Device updateDevice(Device device){
+        return dao.save(device);
 
-    public DeviceService(){
-
+    }
+    public void deleteDevice(Integer id){
+        dao.deleteById(id);
     }
 }
